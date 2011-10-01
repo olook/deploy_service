0ccc31398c32aa9c090b9c986517b3c023b00229
zanst <me@zan.st>
2011-10-01 00:49:05 -0300

    added 'checkout master' to ensure that the pull will work

--- app.rb
+++ app.rb
@@ -15,9 +15,7 @@ post '/deploy' do
   repo_ref = received_params["ref"]
 
   if repo_ref.include?("tags/homolog")
-    `cd #{options.app_path} && git pull && git checkout #{repo_ref} && rake db:migrate`
-  elsif repo_ref.include?("tags/prod")
-    # Will be implemented when the production environment begin to be assembled.
+    `cd #{options.app_path} && git checkout master && git pull && git checkout #{repo_ref} && rake db:migrate`
   end
 
 end
