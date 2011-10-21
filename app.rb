d2e0d43c753eff10ef2103411a8065bfed9e23d8
zanst <me@zan.st>
2011-10-21 12:25:33 -0200

    fixing up

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ post '/deploy' do
   repo_ref = received_params["ref"]
 
   if repo_ref.include?("tags/homolog")
-    `cd #{options.app_path} && git add . && git commit -m "preparing for tag changing" && git checkout master && git pull && git checkout #{repo_ref} && rake db:migrate`
+    `cd #{options.app_path} && git add . && git commit -m "preparing for tag changing" && git checkout master && git pull && git checkout #{repo_ref} && bundle install && rake db:migrate RAILS_ENV=production && init_production`
   end
 
 end
@@ -27,6 +27,7 @@ get '/rollback' do
 end
 
 post '/rollback_tag' do
-  `cd #{options.app_path} && git add . && git commit -m "preparing for tag changing" && git checkout master && git pull && git checkout #{params[:tag_version]} && rake db:migrate`
+  `cd #{options.app_path} && git add . && git commit -m "preparing for tag changing" && git checkout master && git pull && git checkout #{params[:tag_version]} && bundle install && rake db:migrate RAILS_ENV=production && init_production`
   erb :rollback_tag
 end
+
