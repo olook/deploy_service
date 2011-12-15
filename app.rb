b805433d116a4e926dab1b8aef8ed506a568a307
zanst <me@zan.st>
2011-12-15 15:04:44 -0200

    commented rollback related stuff for now

--- app.rb
+++ app.rb
@@ -9,14 +9,14 @@ post '/deploy' do
   DeliveryBus::Deploy.new.dispatcher(params[:payload])
 end
 
-get '/rollback' do
-  @tags = `cd #{options.app_path} && git tag | grep homolog`.split("\n")
-  erb :rollback
-end
-
-post '/rollback_tag' do
-  @tag_version = params[:tag_version]
-  `cd #{options.app_path} && git add . && git checkout master && git pull && git checkout #{@tag_version} && bundle install && rake db:drop RAILS_ENV=production && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production && init_unicorn`
-  erb :rollback_tag
-end
+#get '/rollback' do
+#  @tags = `cd #{options.app_path} && git tag | grep homolog`.split("\n")
+#  erb :rollback
+#end
+#
+#post '/rollback_tag' do
+#  @tag_version = params[:tag_version]
+#  `cd #{options.app_path} && git add . && git checkout master && git pull && git checkout #{@tag_version} && bundle install && rake db:drop RAILS_ENV=production && rake db:create RAILS_ENV=production && rake db:migrate RAILS_ENV=production && init_unicorn`
+#  erb :rollback_tag
+#end
 
