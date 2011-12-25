f8d55ff336864a110361d9679201f96d77193fed
zanst <me@zan.st>
2011-12-25 03:51:13 -0200

    improved DeliveryBus::Trigger

--- lib/delivery_bus/trigger.rb
+++ lib/delivery_bus/trigger.rb
@@ -1,6 +1,7 @@
+# -*- encoding: utf-8 -*-
 module DeliveryBus
   class Trigger
-    attr_reader :app_servers, :action_type
+    attr_reader :app_servers
 
     def initialize
       config_path  = File.dirname(__FILE__) + '/../../config/deploy.yml'
@@ -9,20 +10,18 @@ module DeliveryBus
     end
 
     def pull!(action_type)
-      action = process_action(action_type)
-      post_to_apps(action)
+      process_action(action_type)
     end
 
     def process_action(action_type)
-      action = { :type => action_type, :date => Time.now + 200 }
-      action.to_json
+      @app_servers.each do |server_address, seconds|
+        action_json = { :type => action_type, :date => Time.now + seconds.to_i }.to_json
+        send_package(server_address, action_json)
+      end
     end
 
-    def post_to_apps(action)
-      @app_servers.each do |app_server|
-        Curl::Easy.http_post(app_server, action)
-      end
+    def send_package(server_address, json)
+      Curl::Easy.http_post(server_address, json)
     end
   end
 end
-
